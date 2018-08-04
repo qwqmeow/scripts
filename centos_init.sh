e365cc43c9a2c2c7533a5bd9fb5e71619bedcc37
qwqmeow <hr@linux.com>
2018-08-04 15:23:35 +0800

    centos_init.sh

---
+++ centos_init.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+sudo ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+sudo setenforce 0
+sudo sed -i 's\enforcing\disabled\g' /etc/selinux/config
+sudo yum install -y epel-release
+sudo yum install -y git gcc-c++ wget curl htop nload python2-pip
+pip install speedtest-cli
