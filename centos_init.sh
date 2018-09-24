385dc43f29ffc9e4b6ebf7749e65d7f473f47b37
qwqmeow <root@meoww.app>
2018-09-24 10:55:19 +0800

    add new file

--- centos_init.sh
+++ centos_init.sh
@@ -3,5 +3,5 @@ sudo ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 sudo setenforce 0
 sudo sed -i 's\enforcing\disabled\g' /etc/selinux/config
 sudo yum install -y epel-release
-sudo yum install -y git gcc-c++ wget curl htop nload python2-pip
+sudo yum install -y git gcc-c++ wget curl htop nload python2-pip vim
 pip install speedtest-cli
